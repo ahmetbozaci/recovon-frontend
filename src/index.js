671c80b316ef7bdc1bfbcda1659a16f5eb074120
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-03-01 10:48:16 +0100

    f

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './app.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
@@ -12,4 +11,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
