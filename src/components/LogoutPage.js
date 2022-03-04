02644df12d82ee3763d13c195de3209873bb6f8d
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-03-04 08:20:09 +0100

    Add logout rest page and try to fetch docs pictures from api

--- src/components/LogoutPage.js
+++ src/components/LogoutPage.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../app.css';
 
 function Logout() {
   return (
-    <div>Logout</div>
+    <section className="mainlogout">
+      <div className="lnav">
+        <ul className="lls">
+          <li className="logo">
+            <img src="ddfdfadf" alt="logo" />
+          </li>
+          <li>login</li>
+          <li>signup</li>
+          <Link to="/" className="nav-link">temporal home  </Link>
+        </ul>
+      </div>
+      <div className="welcomelg">
+        <h1 className="hg">Health-Guru</h1>
+        <h2 className="hg2">Health is wealth, he who knows this knows peace </h2>
+        <h1 className="hg3">Be HAPPY, Be HEALTHY</h1>
+
+        <button type="button">learn-more</button>
+        <button type="button">Begin your experience with Health-Guru</button>
+      </div>
+    </section>
   );
 }
 
