c56fc8638d50bc595573371fd05fda076cb15f29
ali abbani <aliox@ahlam.com>
2022-03-05 18:42:05 +0200

    remove uncommented code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,20 +6,6 @@ import LogOut from './LogOut';
 import '../assets/style/nav.css';
 
 const NavBar = () => (
-  // <div className="Navigation1">
-  //   <ul className="navbar-nav mr-auto">
-  //     <li className="logo">
-  //       <img src="ddfdfadf" alt="logo" />
-  //     </li>
-  //     <li><Link to="/" className="nav-link"> Doctors </Link></li>
-  //     <li><Link to="/myappointments" className="nav-link">My Appointments</Link></li>
-  //   </ul>
-  //   <LogOut />
-  //   <div>
-  //
-  //   </div>
-  // </div>
-
   <nav className="navbar navbar-expand navbar-dark bg-primary fixed-left">
     <a className="navbar-brand" href>Logo</a>
 
@@ -37,7 +23,6 @@ const NavBar = () => (
       </ul>
     </div>
   </nav>
-
 );
 
 export default NavBar;
