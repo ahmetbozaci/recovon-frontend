06a177b9139c6efa41c66c8b6a3078ad5633655d
ali abbani <aliox@ahlam.com>
2022-03-05 13:57:05 +0200

    Style the login page

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
@@ -41,32 +42,54 @@ const LoginForm = () => {
   };
 
   return (
-    <div>
-      <h1>Logo</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Email"
-          name="email"
-          value={email}
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          name="password"
-          value={password}
-          onChange={handleChange}
-        />
-        <button type="submit">Log In</button>
-      </form>
-      <div>
-        <p>Or</p>
-        <button type="submit" onClick={() => navigate('/signup')}>
-          Sign up
-        </button>
+    <section className="vh-100" style={{ backgroundColor: '#1266f1' }}>
+      <div className="container py-3 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div className="card shadow-2-strong" style={{ borderRadius: '1rem' }}>
+              <div className="card-body p-5 text-center">
+                <h3 className="mb-5">Sign in</h3>
+                <form onSubmit={handleSubmit}>
+                  <label className="form-label">
+                    Email Address
+                    <input
+                      className="form-control form-control-lg"
+                      style={{ borderRadius: '1rem' }}
+                      type="email"
+                      placeholder="Email"
+                      name="email"
+                      value={email}
+                      onChange={handleChange}
+                    />
+                  </label>
+                  <input
+                    className="form-control form-control-lg"
+                    style={{ borderRadius: '1rem' }}
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    value={password}
+                    onChange={handleChange}
+                  />
+                  <button
+                    type="submit"
+                    className="btn btn-primary btn-lg btn-block"
+                  >
+                    Log In
+                  </button>
+                </form>
+                <div>
+                  <p>Or</p>
+                  <button type="submit" onClick={() => navigate('/signup')}>
+                    Sign up
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
