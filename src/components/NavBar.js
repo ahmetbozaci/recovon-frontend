63ed071e1d65f9d53da94561dcad7ec20941cda4
ali abbani <aliox@ahlam.com>
2022-03-07 11:19:02 +0200

    Revert

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import Footer from './Footer';
+import Footer from './Footer';
 import LogOut from './LogOut';
-// import '../app.css';
-import '../assets/style/nav.css';
+import '../app.css';
 
 const NavBar = () => (
-  <nav className="navbar navbar-expand navbar-dark bg-primary fixed-left">
-    <a className="navbar-brand" href>Logo</a>
+  <div className="Navigation1">
+    <ul className="navbar-nav mr-auto">
+      <li className="logo">
+        <img src="ddfdfadf" alt="logo" />
+      </li>
+      <li><Link to="/" className="nav-link"> Doctors </Link></li>
+      <li><Link to="/myappointments" className="nav-link">My Appointments</Link></li>
+      <li><Link to="/appointment" className="nav-link">Appointment</Link></li>
 
-    <div className="collapse navbar-collapse">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link to="/" className="nav-link">Doctors</Link>
-        </li>
-        <li className="">
-          <Link to="/myappointments" className="nav-link">My Appointments</Link>
-        </li>
-        <li className="pt-5 pl-5">
-          <LogOut />
-        </li>
-      </ul>
+    </ul>
+    <LogOut />
+    <div>
+      <Footer />
     </div>
-  </nav>
+  </div>
+
 );
 
 export default NavBar;
