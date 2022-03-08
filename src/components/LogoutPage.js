0a00299877b4d80b66ed22d73073689be1561d4e
ali abbani <aliox@ahlam.com>
2022-03-09 00:40:26 +0200

    add button

--- src/components/LogoutPage.js
+++ src/components/LogoutPage.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../app.css';
+// import '../app.css';
+import '../assets/style/logoutPage.css';
 
 function Logout() {
   return (
     <section className="mainlogout">
-
       <div className="welcomelg">
-        <div className="hg">
-          {/* <h1 className="hg">Docstagram</h1> */}
-        </div>
         {/* <div className="hg3">
           <h1>Be HAPPY,</h1>
           <h1> Be HEALTHY</h1>
         </div> */}
-        <button className="downbtn" type="button"><Link to="signup" className="createapp"> Start your health experience </Link></button>
+        <Link to="login"><button type="button" className="btn btn-primary first-page-button"> Start your health experience </button></Link>
       </div>
     </section>
   );
