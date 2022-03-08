c0b8d55e0230b96234770fd4527fd879eb6223f5
ali abbani <aliox@ahlam.com>
2022-03-09 00:29:42 +0200

    fix the nav bar and change the backg pic

--- src/components/LogoutPage.js
+++ src/components/LogoutPage.js
@@ -1,29 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../app.css';
-import logo from '../assets/images/logo.jpg';
 
 function Logout() {
   return (
     <section className="mainlogout">
-      <div className="Navigation1">
-        <ul className="navbar-nav mr-auto">
-          <li className="logo">
-            <img className="imgg" src={logo} alt="logo" />
-          </li>
-          <li><Link to="login" className="dnav-link"> login </Link></li>
-          <li><Link to="signup" className="anav-link">signup</Link></li>
-        </ul>
-      </div>
 
       <div className="welcomelg">
         <div className="hg">
-          <h1 className="hg">Docstagram</h1>
+          {/* <h1 className="hg">Docstagram</h1> */}
         </div>
-        <div className="hg3">
+        {/* <div className="hg3">
           <h1>Be HAPPY,</h1>
           <h1> Be HEALTHY</h1>
-        </div>
+        </div> */}
         <button className="downbtn" type="button"><Link to="signup" className="createapp"> Start your health experience </Link></button>
       </div>
     </section>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Footer from './Footer';
 import LogOut from './LogOut';
-import '../app.css';
-import logo from '../assets/images/logo.jpg';
+import '../assets/style/nav.css';
+// import Footer from './Footer';
+// import '../app.css';
+// import logo from '../assets/images/logo.jpg';
 
 const NavBar = () => (
-  <div className="Navigation1">
-    <ul className="navbar-nav">
-      <li className="logo">
-        <img className="imgg" src={logo} alt="logo" />
-      </li>
-      <li><Link to="/" className="dnav-link"> Doctors </Link></li>
-      <li><Link to="/myappointments" className="anav-link">Appointments</Link></li>
-    </ul>
-    <LogOut />
-    <div>
-      <Footer />
-    </div>
-  </div>
+  // <div className="Navigation1">
+  //   <ul className="navbar-nav">
+  //     <li className="logo">
+  //       <img className="imgg" src={logo} alt="logo" />
+  //     </li>
+  //     <li><Link to="/" className="dnav-link"> Doctors </Link></li>
+  //     <li><Link to="/myappointments" className="anav-link">Appointments</Link></li>
+  //   </ul>
+  //   <LogOut />
+  //   <div>
+  //     <Footer />
+  //   </div>
+  // </div>
 
+  <nav className="navbar navbar-expand navbar-dark bg-primary fixed-left">
+    <a className="navbar-brand" href>Logo</a>
+
+    <div className="collapse navbar-collapse">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link to="/" className="nav-link">Doctors</Link>
+        </li>
+        <li className="">
+          <Link to="/myappointments" className="nav-link">My Appointments</Link>
+        </li>
+        <li className="pt-5 pl-5">
+          <LogOut />
+        </li>
+      </ul>
+    </div>
+  </nav>
 );
 
 export default NavBar;
