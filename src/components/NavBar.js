ddc599d5a1b2aed7a5e405e1d2911842f0282460
ali abbani <aliox@ahlam.com>
2022-03-09 13:36:33 +0200

    Fix the logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import LogOut from './LogOut';
 import '../assets/style/nav.css';
+import recovon from '../assets/images/recovon-logos-transparent.png';
 // import Footer from './Footer';
 // import '../app.css';
 // import logo from '../assets/images/logo.jpg';
@@ -21,18 +22,19 @@ const NavBar = () => (
   //   </div>
   // </div>
 
-  <nav className="navbar nav-mobile navbar-expand navbar-dark bg-primary fixed-left">
-    <a className="navbar-brand" href>Logo</a>
+  <nav className="navbar nav-mobile navbar-expand bg-primary fixed-left">
+    <img src={recovon} alt="recovon logo" className="nav-logo mx-auto" />
+    <h2 className="nav-mobile-logo">Recovon</h2>
 
-    <div className="collapse navbar-collapse">
+    <div className="collapse navbar-collapse nav-element-space">
       <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link to="/doctors" className="nav-link">Doctors</Link>
+        <li className="">
+          <Link to="/doctors" className="nav-link nav-element-color">Doctors</Link>
         </li>
         <li className="">
-          <Link to="/myappointments" className="nav-link">My Appointments</Link>
+          <Link to="/myappointments" className="nav-link nav-element-color">My Appointments</Link>
         </li>
-        <li className="">
+        <li className="nav-logout-space">
           <LogOut style={{ marginLeft: '' }} />
         </li>
       </ul>
