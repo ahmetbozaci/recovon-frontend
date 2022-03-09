7051eaa7324664732e52db57c499c0b240bf6886
ali abbani <aliox@ahlam.com>
2022-03-09 22:07:41 +0200

    Add doctor name inside my app page

--- src/components/MyAppointment.js
+++ src/components/MyAppointment.js
@@ -7,7 +7,7 @@ import '../assets/style/myApp.css';
 // import fetchDoctor from '../Redux/Doctor/doctorActions';
 
 const MyAppointment = () => {
-  const myAppData = useSelector((state) => state.myAppReducer.myApp); //contain doctor_id
+  const myAppData = useSelector((state) => state.myAppReducer.myApp); // contain doctor_id
   const doctorData = useSelector((state) => state.doctorReducer.doctors); // contain id AS doctor_id
   // console.log('doctor data inside appointment', doctorData);
 
@@ -18,45 +18,45 @@ const MyAppointment = () => {
     dispatch(fetchMyApp());
   }, []);
 
-  
-
   return (
     <div className="p-2 myAppMargin">
       <h1 className="p-2 text-center">My Appointments</h1>
       <div>
         {myAppData
-                && myAppData.map((d) => (
-                  <div className="card mb-3 p-2" key={d.id}>
-                    <div className="row g-0">
-                    <div>
-                      The user is <b>{isLoggedIn ? 'currently' : 'not'}</b> logged in.
-                    </div>
-                      <div className="col-md-4">
-                        <h2>
-                          {doctorData.map((doctor) => {
-                            {doctor.id === d.id ? <p>Doctor name: {doctor.name}</p> : <p />}
-                              
-                          })}
-                        </h2>
-                      </div>
-                      <div className="col-md-8">
-                        <div className="card-body">
-                          {/* <h5 className="card-title">Card title</h5> */}
-                          <p className="card-text" style={{ fontSize: '1.5rem' }}>
-                            Reservation Date:
+                && myAppData.map((d) => {
+                  const { doctor_id: doctorId } = d;
+                  const currentDoctor = doctorData.filter((doctor) => doctor.id === doctorId);
+                  console.log('here is the current doctor', currentDoctor);
+                  console.log('here is a doctor id', doctorId);
+                  return (
+                    <div className="card mb-3 p-2" key={d.id}>
+                      <div className="row g-0">
+                        <div className="col-md-4">
+                          <h4>
+                            Doctor Name:
                             {' '}
-                            <small className="text-muted">{d.date}</small>
-                          </p>
-                          <p className="card-text" style={{ fontSize: '1.5rem' }}>
-                            Reservation Time:
-                            {' '}
-                            <Moment format="HH:MM" className="text-muted">{d.time}</Moment>
-                          </p>
+                            {currentDoctor[0].name}
+                          </h4>
+                        </div>
+                        <div className="col-md-8">
+                          <div className="card-body">
+                            {/* <h5 className="card-title">Card title</h5> */}
+                            <p className="card-text" style={{ fontSize: '1.5rem' }}>
+                              Reservation Date:
+                              {' '}
+                              <small className="text-muted">{d.date}</small>
+                            </p>
+                            <p className="card-text" style={{ fontSize: '1.5rem' }}>
+                              Reservation Time:
+                              {' '}
+                              <Moment format="HH:MM" className="text-muted">{d.time}</Moment>
+                            </p>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                ))}
+                  );
+                })}
       </div>
     </div>
 
