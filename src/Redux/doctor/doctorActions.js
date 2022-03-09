945c4381cb4dec0b7930eedd0d5582bd90f5c0b2
Ahmet Bozaci <ahmt9417@gmail.com>
2022-03-09 17:55:37 +0300

    Add one doctor reducer, action

--- src/Redux/doctor/doctorActions.js
+++ src/Redux/doctor/doctorActions.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_DOCTORS_SUCCESS = 'FETCH_DOCTORS_SUCCESS';
+export const FETCH_ONE_DOCTOR_SUCCESS = 'FETCH_ONE_DOCTOR_SUCCESS';
 
 const baseURL = 'https://final-api-55.herokuapp.com/doctors';
 
@@ -17,4 +18,16 @@ const fetchDoctors = () => (dispatch) => {
     });
 };
 
+const fetchOneDoctorSuccess = (doctor) => ({
+  type: FETCH_ONE_DOCTOR_SUCCESS,
+  payload: doctor,
+});
+
+export const fetchOneDoctor = (id) => (dispatch) => {
+  axios.get(`${baseURL}/${id}`)
+    .then((response) => {
+      const doctor = response.data;
+      dispatch(fetchOneDoctorSuccess(doctor));
+    });
+};
 export default fetchDoctors;
--- src/components/Doctor.js
+++ src/components/Doctor.js
@@ -1,40 +1,44 @@
-import { useSelector } from 'react-redux';
+/** @format */
+
+import { useSelector, useDispatch } from 'react-redux';
 import '../assets/style/doctor.css';
 import { Carousel } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { fetchOneDoctor } from '../Redux/doctor/doctorActions';
 
 const Doctor = () => {
+  const dispatch = useDispatch();
   const doctor = useSelector((state) => state.doctorReducer);
   const { doctors } = doctor;
 
   if (doctors.length === 0) {
-    return (
-      <h3>....</h3>
-    );
+    return <h3>....</h3>;
   }
+
+  const handleClick = (event) => {
+    const { id } = event.target;
+    dispatch(fetchOneDoctor(id));
+  };
 
   return (
     <Carousel className="caro doctorMargin">
-      {
-        doctors.map((doc) => (
-          <Carousel.Item interval={2000} key={doc.id} className="maincaro">
-            <img
-              className="docimg"
-              src={doc.picture}
-              alt="First slide"
-            />
-            <Carousel.Caption className="caption">
-              <h3 className="docname">
-                Dr .
-                {doc.name}
-              </h3>
-              <button className="docbtn" type="button"><Link to="/appointment" className="createapp"> Create-Appointment </Link></button>
-
-            </Carousel.Caption>
-          </Carousel.Item>
-        ))
-      }
-
+      {doctors.map((doc) => (
+        <Carousel.Item interval={2000} key={doc.id} className="maincaro">
+          <img className="docimg" src={doc.picture} alt="First slide" />
+          <Carousel.Caption className="caption">
+            <h3 className="docname">
+              Dr .
+              {doc.name}
+            </h3>
+            <button className="docbtn" type="button" onClick={handleClick}>
+              <Link to="/appointment" className="createapp" id={doc.id}>
+                {' '}
+                Create-Appointment
+              </Link>
+            </button>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
     </Carousel>
   );
 };
