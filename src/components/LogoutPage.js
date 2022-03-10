93ce9da21ac91f4a8c4a162fb3222e458af128d5
ali abbani <aliox@ahlam.com>
2022-03-10 11:26:54 +0200

    Add link to image

--- src/components/LogoutPage.js
+++ src/components/LogoutPage.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 // import '../app.css';
 import '../assets/style/logoutPage.css';
-import recovon from '../assets/images/recovon-logos-transparent.png';
+import recovonFirst from '../assets/images/recovon-logos-transparent.png';
 
 function Logout() {
   return (
     <section className="mainlogout">
       <div className="first-page-logo-div">
-        <img src={recovon} alt="recovon logo" className="first-page-logo" />
+        <Link to="/login"><img src={recovonFirst} alt="recovon logo" className="first-page-logo" /></Link>
       </div>
       <div className="logoutButton text-center">
         <Link to="login"><button type="button" className="btn text-center first-page-button"> Start your health experience </button></Link>
