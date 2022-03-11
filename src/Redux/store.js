8b066b42998760dbe6d57e884f62e2279e7e4572
ali abbani <aliabbani08@gmail.com>
2022-03-11 11:58:21 +0200

    Add Redux DevTools Extension compose with dev tools inside the store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './rootReducer';
 import fetchDoctors from './doctor/doctorActions';
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(logger, thunk),
+  composeWithDevTools(applyMiddleware(logger, thunk)),
 );
 store.dispatch(fetchDoctors());
 
